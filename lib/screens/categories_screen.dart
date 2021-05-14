f617d885c103e76aba055e32d151b82642bcbb75
arthur de oliveira ribeiro <arthuroliveiraribeiro9@gmail.com>
2021-05-14 09:44:53 -0300

    novo estilo

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
-
-
+import '../components/category_item.dart';
+import '../data/dummy_data.dart';
 //Aqui vai ser a tela de categorias 
 class CategoriasScreen extends StatelessWidget {
   @override
   Widget  build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200, //cada elemento só pode 200px
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20, 
-        mainAxisExtent: 20,
+    return Scaffold(
+          appBar: AppBar(
+            title: Text('Vamos cozinhar?'),
+          ),
+          body: GridView(
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200, //cada elemento só pode 200px
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20, 
+              mainAxisExtent: 20,
 
+          ),
+        children: DUMMY_CATEGORIES.map((cat){
+          //na tela vou mostra pela categoria que estou passando nesse momento
+          // retornando em cada item da lista o estilo da category item
+          return CategoryItem(cat);
+        }).toList(),
       ),
-      children: <Widget>[
-        
-      ],
     );
   }
 }
