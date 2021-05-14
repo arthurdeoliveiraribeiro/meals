3c426408e332814387766df2ffbc972e76d9b9b6
arthur de oliveira ribeiro <arthuroliveiraribeiro9@gmail.com>
2021-05-14 10:59:29 -0300

    fontes

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,8 +1,9 @@
+
 import 'package:flutter/material.dart';
 import '../components/category_item.dart';
 import '../data/dummy_data.dart';
 //Aqui vai ser a tela de categorias 
-class CategoriasScreen extends StatelessWidget {
+class CategoriesScreen extends StatelessWidget {
   @override
   Widget  build(BuildContext context) {
     return Scaffold(
@@ -10,19 +11,20 @@ class CategoriasScreen extends StatelessWidget {
             title: Text('Vamos cozinhar?'),
           ),
           body: GridView(
+            padding: EdgeInsets.all(15) ,
             gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
               maxCrossAxisExtent: 200, //cada elemento só pode 200px
               childAspectRatio: 3 / 2,
               crossAxisSpacing: 20, 
-              mainAxisExtent: 20,
+              mainAxisSpacing: 20,
 
           ),
-        children: DUMMY_CATEGORIES.map((cat){
-          //na tela vou mostra pela categoria que estou passando nesse momento
-          // retornando em cada item da lista o estilo da category item
-          return CategoryItem(cat);
-        }).toList(),
-      ),
+          children: DUMMY_CATEGORIES.map((cat){
+            //na tela vou mostra pela categoria que estou passando nesse momento
+            // retornando em cada item da lista o estilo da category item
+            return CategoryItem(cat);
+          }).toList(),
+        ),
     );
   }
 }
