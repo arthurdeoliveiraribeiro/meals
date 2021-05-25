0e4ecc5c3c94375a53fcd8fbbc383b5f06b342f6
arthur de oliveira ribeiro <arthuroliveiraribeiro9@gmail.com>
2021-05-25 16:24:42 -0300

    dividindo as telas
    categoria e favorito

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,37 +2,54 @@ import 'package:flutter/material.dart';
 import 'categories_screen.dart';
 import 'favorite_screen.dart';
 
-class TabsScreen extends StatelessWidget {
+class TabsScreen extends StatefulWidget {
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  
+  int _selectedScreenIndex = 0;
+  final List<Map<String, Object>> _screens = [
+      {'title':'Lista de Categorias', 'screen': CategoriesScreen()},
+      {'title':'Meus favoritos', 'screen': FavoriteScreen()},
+      //Lista de telas
+  ];
+
+  _selectScreen( int index){
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Vamos Cozinhar?'),
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categorias',
-              ),
-              Tab(
-                icon: Icon(Icons.favorite),
-                text: 'Favoritos',
-              )
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: [
-            CategoriesScreen(),
-            FavoriteScreen()
-          ],
-        )
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_screens[_selectedScreenIndex]['title']),
       ),
-      //Eu digo quantas abas tem e passo elas 
-      // passei duas abas uma de favorito e outra de categorias 
+      body: _screens[_selectedScreenIndex]['screen'],//começando com a tela 0
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectScreen,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedScreenIndex,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.food_bank),
+            title: Text('Categorias')
+
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_outline),
+            title: Text('Favoritos')
+
+          ),
+        ],
+      ),
+      
     );
     
   }
